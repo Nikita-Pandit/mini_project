6872de2272cc1d8ef3d8f33f8f99a842f5944ffc
Nikita Pandit <nikitapandit651@gmail.com>
2024-12-05 14:37:58 +0530

    Added about us page

--- universityProject/src/App.jsx
+++ universityProject/src/App.jsx
@@ -6,15 +6,15 @@ import SignUp from './pages/SignUp';
 import Home from "./pages/Home"
 import Projects from "./pages/Projects"
 import ResearchDoubts from "./pages/ResearchDoubts"
-import Profile from './pages/Profile';
+import Profile from './pages/profile';
 import Body from './components/body';
 const App = () => {
   return (
    <Router>
     <StudentHeader/>
     <Routes>
-      <Route path="/Profile" element={<Profile/>}></Route>
     <Route path="/" element={<Home/>}/>
+    <Route path="/Profile" element={<Profile/>}/>
     <Route path="/Projects" element={<Projects/>}/>
     <Route path="/SignUp" element={<SignUp/>}/>
       <Route path="/ResearchDoubts" element={<ResearchDoubts/>}/>
--- universityProject/src/pages/ResearchDoubts.jsx
+++ universityProject/src/pages/ResearchDoubts.jsx
@@ -2,9 +2,63 @@ import React from 'react'
 
 const ResearchDoubts = () => {
   return (
-    <div>
-      <h1>Hello, ResearchDoubts</h1>
+    <>
+     <div className='flex items-center justify-center mt-20' >
+     <input className="bg-zinc-500  p-3 border-2 rounded-md outline-none border-none text-center" type="text" required placeholder='Input keywords' /> 
+     </div>
+
+     <div className='p-5 bg-red-500'>
+  <div className='p-5 bg-blue-500  flex flex-row items-center justify-center flex-wrap gap-5'>
+
+    <div className='bg-zinc-500 p-5 card'>
+      <img className='' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9SRRmhH4X5N2e4QalcoxVbzYsD44C-sQv-w&s" alt="" />
+   
+     <p>Web development, AI/ML, App Development</p>
+    <div className='flex items-center justify-center'>
+    <button type="button" class="more-info text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">View more details</button>
     </div>
+    </div>
+
+    <div className='bg-zinc-500 p-5 card'>
+      <img className='' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9SRRmhH4X5N2e4QalcoxVbzYsD44C-sQv-w&s" alt="" />
+     <h1 className="student-name">Nikita Pandit</h1>
+     <p>Web development, AI/ML, App Development</p>
+     <div className='flex items-center justify-center'>
+    <button type="button" class="more-info text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">View more details</button>
+    </div>
+    </div> 
+
+    <div className='bg-zinc-500 p-5 card'>
+      <img className='' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9SRRmhH4X5N2e4QalcoxVbzYsD44C-sQv-w&s" alt="" />
+     <h1 className="student-name">Nikita Pandit</h1>
+     <p>Web development, AI/ML, App Development</p>
+     <div className='flex items-center justify-center'>
+    <button type="button" class="more-info text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">View more details</button>
+    </div>
+    </div>
+
+    <div className='bg-zinc-500 p-5 card'>
+      <img className='' src="https://c8.alamy.com/comp/K3K98H/japan-hoshu-tokyo-akihabara-artwork-depicting-cosplay-cartoon-characters-K3K98H.jpg" alt="" />
+     <h1 className="student-name">Nikita Pandit</h1>
+     <p>Web development, AI/ML, App Development</p>
+     <div className='flex items-center justify-center'>
+    <button type="button" class="more-info text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">View more details</button>
+    </div>
+    </div>
+
+    <div className='bg-zinc-500 p-5 card'>
+      <img className='' src="https://www.fluentu.com/blog/wp-content/uploads/site//6/top-5-japanese-kids-cartoons-to-boost-language-skills-doraemon.jpg" alt="" />
+     <h1 className="student-name">Nikita Pandit</h1>
+     <p>Web development, AI/ML, App Development</p>
+     <div className='flex items-center justify-center'>
+    <button type="button" class="more-info text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">View more details</button>
+    </div>
+    </div>
+
+    
+  </div>
+     </div>
+    </>
   )
 }
 
