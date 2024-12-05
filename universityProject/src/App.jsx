d23fc7c53cb2f438bc2d8988611beb3b008c0ea9
Nikita Pandit <nikitapandit651@gmail.com>
2024-12-05 14:54:00 +0530

    Added login page

--- universityProject/src/App.jsx
+++ universityProject/src/App.jsx
@@ -8,15 +8,20 @@ import Projects from "./pages/Projects"
 import ResearchDoubts from "./pages/ResearchDoubts"
 import Profile from './pages/profile';
 import Body from './components/body';
+import AboutUs from './pages/AboutUs';
+import Login from './pages/Login';
 const App = () => {
+
   return (
    <Router>
     <StudentHeader/>
     <Routes>
     <Route path="/" element={<Home/>}/>
+    <Route path="/AboutUs" element={<AboutUs/>}/>
     <Route path="/Profile" element={<Profile/>}/>
     <Route path="/Projects" element={<Projects/>}/>
     <Route path="/SignUp" element={<SignUp/>}/>
+    <Route path="/Login" element={<Login/>}/>
       <Route path="/ResearchDoubts" element={<ResearchDoubts/>}/>
     </Routes>
     <Body/>
