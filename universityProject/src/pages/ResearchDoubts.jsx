b5ea1df45db3ef6e8496b7d969241460570f7726
Simran Shah <shahsimran453@gmail.com>
2024-12-06 16:14:14 +0530

    updated cards

--- universityProject/src/pages/ResearchDoubts.jsx
+++ universityProject/src/pages/ResearchDoubts.jsx
@@ -2,60 +2,117 @@ import React from 'react'
 
 const ResearchDoubts = () => {
   return (
-    <>
-     <div className='flex items-center justify-center mt-20' >
-     <input className="bg-zinc-500  p-3 border-2 rounded-md outline-none border-none text-center" type="text" required placeholder='Input keywords' /> 
+<>
+     <div className='flex items-center justify-center' >
+     <input className="bg-zinc-500 input-field p-3 border-2 rounded-md outline-none border-none text-center" type="text" required placeholder='Input keywords' /> 
      </div>
 
-     <div className='p-5 bg-red-500'>
-  <div className='p-5 bg-blue-500  flex flex-row items-center justify-center flex-wrap gap-5'>
+     <div className='p-5'>
+     <div className='p-5 justify-center flex flex-row flex-wrap gap-5'>
 
-    <div className='bg-zinc-500 p-5 card'>
-      <img className='' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9SRRmhH4X5N2e4QalcoxVbzYsD44C-sQv-w&s" alt="" />
-   
+    <div className='bg-zinc-500 card p-3'>
+    
+    <div className='image-profile-container  border rounded-md'>
+ <img className='rounded-md w-full  h-full object-cover' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWejx1A1AUS1FgggjfcC-4iUXMg7C-GaJdrQ&s" alt="" /> 
+</div>
+
+      <h1 className="student-name">Nikita Pandit</h1>
      <p>Web development, AI/ML, App Development</p>
     <div className='flex items-center justify-center'>
     <button type="button" class="more-info text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">View more details</button>
     </div>
     </div>
 
-    <div className='bg-zinc-500 p-5 card'>
-      <img className='' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9SRRmhH4X5N2e4QalcoxVbzYsD44C-sQv-w&s" alt="" />
-     <h1 className="student-name">Nikita Pandit</h1>
-     <p>Web development, AI/ML, App Development</p>
-     <div className='flex items-center justify-center'>
-    <button type="button" class="more-info text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">View more details</button>
-    </div>
-    </div> 
-
-    <div className='bg-zinc-500 p-5 card'>
-      <img className='' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9SRRmhH4X5N2e4QalcoxVbzYsD44C-sQv-w&s" alt="" />
-     <h1 className="student-name">Nikita Pandit</h1>
-     <p>Web development, AI/ML, App Development</p>
-     <div className='flex items-center justify-center'>
-    <button type="button" class="more-info text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">View more details</button>
-    </div>
-    </div>
-
-    <div className='bg-zinc-500 p-5 card'>
-      <img className='' src="https://c8.alamy.com/comp/K3K98H/japan-hoshu-tokyo-akihabara-artwork-depicting-cosplay-cartoon-characters-K3K98H.jpg" alt="" />
-     <h1 className="student-name">Nikita Pandit</h1>
-     <p>Web development, AI/ML, App Development</p>
-     <div className='flex items-center justify-center'>
-    <button type="button" class="more-info text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">View more details</button>
-    </div>
-    </div>
-
-    <div className='bg-zinc-500 p-5 card'>
-      <img className='' src="https://www.fluentu.com/blog/wp-content/uploads/site//6/top-5-japanese-kids-cartoons-to-boost-language-skills-doraemon.jpg" alt="" />
-     <h1 className="student-name">Nikita Pandit</h1>
-     <p>Web development, AI/ML, App Development</p>
-     <div className='flex items-center justify-center'>
-    <button type="button" class="more-info text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">View more details</button>
-    </div>
-    </div>
-
+    <div className='bg-zinc-500 card p-3'>
     
+    <div className='image-profile-container  border rounded-md'>
+ <img className='rounded-md w-full  h-full object-cover' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJFOMCnPh4TVIS4CG8gp1oUFdA6YlIM2jOIw&s" alt="" /> 
+</div>
+
+      <h1 className="student-name">Nikita Pandit</h1>
+     <p>Web development, AI/ML, App Development</p>
+    <div className='flex items-center justify-center'>
+    <button type="button" class="more-info text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">View more details</button>
+    </div>
+    </div>
+
+    <div className='bg-zinc-500 card p-3'>
+    
+    <div className='image-profile-container  border rounded-md'>
+ <img className='rounded-md w-full  h-full object-cover' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWejx1A1AUS1FgggjfcC-4iUXMg7C-GaJdrQ&s" alt="" /> 
+</div>
+
+      <h1 className="student-name">Nikita Pandit</h1>
+     <p>Web development, AI/ML, App Development</p>
+    <div className='flex items-center justify-center'>
+    <button type="button" class="more-info text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">View more details</button>
+    </div>
+    </div>
+
+    <div className='bg-zinc-500 card p-3'>
+    
+    <div className='image-profile-container  border rounded-md'>
+ <img className='rounded-md w-full  h-full object-cover' src="https://i.pinimg.com/474x/bc/f0/27/bcf0272b061414169e8d2e21659223c7.jpg" alt="" /> 
+</div>
+
+      <h1 className="student-name">Nikita Pandit</h1>
+     <p>Web development, AI/ML, App Development</p>
+    <div className='flex items-center justify-center'>
+    <button type="button" class="more-info text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">View more details</button>
+    </div>
+    </div>
+
+    <div className='bg-zinc-500 card p-3'>
+    
+    <div className='image-profile-container  border rounded-md'>
+ <img className='rounded-md w-full  h-full object-cover' src="https://i.pinimg.com/474x/bc/f0/27/bcf0272b061414169e8d2e21659223c7.jpg" alt="" /> 
+</div>
+
+      <h1 className="student-name">Nikita Pandit</h1>
+     <p>Web development, AI/ML, App Development</p>
+    <div className='flex items-center justify-center'>
+    <button type="button" class="more-info text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">View more details</button>
+    </div>
+    </div>
+
+    <div className='bg-zinc-500 card p-3'>
+    
+    <div className='image-profile-container  border rounded-md'>
+ <img className='rounded-md w-full  h-full object-cover' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWejx1A1AUS1FgggjfcC-4iUXMg7C-GaJdrQ&s" alt="" /> 
+</div>
+
+      <h1 className="student-name">Nikita Pandit</h1>
+     <p>Web development, AI/ML, App Development</p>
+    <div className='flex items-center justify-center'>
+    <button type="button" class="more-info text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">View more details</button>
+    </div>
+    </div>
+
+    <div className='bg-zinc-500 card p-3'>
+    
+    <div className='image-profile-container  border rounded-md'>
+ <img className='rounded-md w-full  h-full object-cover' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJFOMCnPh4TVIS4CG8gp1oUFdA6YlIM2jOIw&s" alt="" /> 
+</div>
+
+      <h1 className="student-name">Nikita Pandit</h1>
+     <p>Web development, AI/ML, App Development</p>
+    <div className='flex items-center justify-center'>
+    <button type="button" class="more-info text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">View more details</button>
+    </div>
+    </div>
+
+    <div className='bg-zinc-500 card p-3'>
+    
+    <div className='image-profile-container  border rounded-md'>
+ <img className='rounded-md w-full  h-full object-cover' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJFOMCnPh4TVIS4CG8gp1oUFdA6YlIM2jOIw&s" alt="" /> 
+</div>
+
+      <h1 className="student-name">Nikita Pandit</h1>
+     <p>Web development, AI/ML, App Development</p>
+    <div className='flex items-center justify-center'>
+    <button type="button" class="more-info text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">View more details</button>
+    </div>
+    </div>
   </div>
      </div>
     </>
